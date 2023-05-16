ce3a508e1d4a5f3648327fb5757ddb954a24e7fe
Nikolas <9225407@gmail.com>
2023-05-16 13:36:36 +0300

    logging testing added

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,9 +1,12 @@
 package org.example.service;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.example.exceptions.UserServiceException;
 import org.example.model.User;
 import org.example.repo.UserRepository;
 
+@Slf4j
 public class UserService {
 
     private final UserRepository repository;
@@ -15,9 +18,11 @@ public class UserService {
     public boolean add(User user){
         if(!repository.findUser(user)){
             Boolean save = repository.tryToSave(user);
+            log.info("Add user {} from server {}", user, this );
             return save;
         } else {
             UserServiceException e = new UserServiceException("user already exists");
+            log.error("User {} already exists!", user);
             throw e;
         }
     }
